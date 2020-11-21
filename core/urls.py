19deacc8b30c4ee5f75665bdfd7d28c5f38e0712
Yulian Prodanov <julianprodanov@icloud.com>
2020-11-22 00:40:41 +0200

    add and stylize sign up and sign in pages

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from core.views import index
+from core.views import index, sign_in, sign_up
 
 urlpatterns = [
     path('', index, name='home'),
+    path('sign-in', sign_in, name="sign in"),
+    path('sign-up', sign_up, name="sign up"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(req):
     return render(req, 'index.html')
+
+
+def sign_in(req):
+    return render(req, 'sign-in.html')
+
+
+def sign_up(req):
+    return render(req, 'sign-up.html')
