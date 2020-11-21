8a009765852ee6442b298683be3f9f58ac07ab02
Yulian Prodanov <julianprodanov@icloud.com>
2020-11-21 02:18:08 +0200

    style landing page and add scroll effect

---
+++ static/js/scripts.js
@@ -0,0 +1,8 @@
+let scrollBtn = document.getElementById('scroll-btn')
+scrollBtn.addEventListener("click", () => {
+// Scroll to a certain element
+    document.querySelector('.carousel-indicators').scrollIntoView({
+        behavior: 'smooth'
+    });
+})
+
