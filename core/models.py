5be470eb828cbd4a87f884949b95b606fd8c5cbf
Yulian Prodanov <julianprodanov@icloud.com>
2020-11-23 15:00:43 +0200

    add pillow package and register post model

--- core/models.py
+++ core/models.py
@@ -5,7 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField(blank=False)
     image = models.ImageField(upload_to='images')
+    git_link = models.URLField()
     tags = models.CharField(max_length=100)
 
-
-
+    def __str__(self):
+        return f"{self.id} - {self.title}"
